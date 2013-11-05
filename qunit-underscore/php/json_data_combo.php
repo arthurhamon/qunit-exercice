6fdceb0891d218e589d5a409e2e29891bf0a2457
arthurhamon <arthur.hamon76@gmail.com>
2013-11-05 15:07:37 +0100

    Final commit

---
+++ qunit-underscore/php/json_data_combo.php
@@ -0,0 +1,9 @@
+<?php
+	$data = array(
+		array('id'=>1,'name'=>'Alexandre','position'=>'student'),
+		array('id'=>2,'name'=>'Matthieu','position'=>'director'),
+		array('id'=>3,'name'=>'Gaspard','position'=>'manager'),
+		array('id'=>4,'name'=>'Herve','position'=>'student')
+	);
+	echo json_encode($data);
+?>
